a0dd19fdb65730938563f5c80da4491b76c86647
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-05 00:24:59 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'El Phonebook',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Poppins"),
       home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
