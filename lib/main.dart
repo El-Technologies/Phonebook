6acf4930a8044855a007b0f7dafbbd78ac275463
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-06 12:03:06 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: ,
       body: SafeArea(
         child: Column(
           children: [
