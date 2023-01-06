89ca49130a830072c31558734d8cb8ea77d5e7b8
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-06 12:17:06 +0100

    commit

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,13 +1,13 @@
 class Contact {
   final String firstName;
-  final String lastName;
-  final String email;
+  final String? lastName;
+  final String? email;
   final String phoneNumber;
 
   Contact({
-    this.firstName,
+    required this.firstName,
     this.lastName,
     this.email,
-    this.phoneNumber,
+    required this.phoneNumber,
   });
 }
