5de76efe2448f47b03ee4a7a5b1dd9978b093612
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-06 20:53:38 +0100

    commit

--- lib/screens/create_contact.dart
+++ lib/screens/create_contact.dart
@@ -24,38 +24,41 @@ class _CreateContactState extends State<CreateContact> {
             ),
             SizedBox(height: 10.h),
             Expanded(
-              child: ListView(
-                children: [
-                  Column(
-                    children: [
-                      Container(
-                        height: 100.w,
-                        width: 100.w,
-                        padding: EdgeInsets.all(20.sp),
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color.fromARGB(255, 206, 219, 230),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: ListView(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          height: 100.w,
+                          width: 100.w,
+                          padding: EdgeInsets.all(20.sp),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromARGB(255, 206, 219, 230),
+                          ),
+                          child: Image.asset("assets/images/add_photo.png"),
                         ),
-                        child: Image.asset("assets/images/add_photo.png"),
-                      ),
-                      SizedBox(height: 10.h),
-                      const Text(
-                        "Add photo",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 9, 92, 160),
+                        SizedBox(height: 10.h),
+                        const Text(
+                          "Add photo",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 9, 92, 160),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20.h),
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.r),
-                      ),
+                      ],
                     ),
-                  )
-                ],
+                    SizedBox(height: 20.h),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             )
           ],
