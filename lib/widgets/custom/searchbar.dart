d8a0ca7af96a885261ad157f28ff3923814b433a
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-06 11:52:18 +0100

    commit

--- lib/widgets/custom/searchbar.dart
+++ lib/widgets/custom/searchbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomSearchBar extends StatefulWidget {
   const CustomSearchBar({super.key});
@@ -12,7 +13,14 @@ class _CustomSearchBarState extends State<CustomSearchBar> {
   Widget build(BuildContext context) {
     return TextField(
       decoration: InputDecoration(
-        
+        border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(13.32.r)),
+        fillColor: const Color.fromARGB(255, 206, 205, 205),
+        filled: true,
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 17.77.h,
+        ),
       ),
     );
   }
