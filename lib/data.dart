c88c31f65ebbce1e05f2867be2902c3010da92b9
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-06 12:32:50 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -2,4 +2,13 @@ import 'contact.dart';
 
 List<Contact> contacts = [
   Contact(firstName: "Aladin", phoneNumber: "+2341234556799"),
+  Contact(firstName: "Sam", phoneNumber: "+2341234556799"),
+  Contact(firstName: "James", phoneNumber: "+2341234556799"),
+  Contact(firstName: "Silver", phoneNumber: "+2341234556799"),
+  Contact(firstName: "Josh", phoneNumber: "+2341234556799"),
+  Contact(firstName: "Carie", phoneNumber: "+2341234556799"),
+  Contact(firstName: "Zen", phoneNumber: "+2341234556799"),
+  Contact(firstName: "Aladin", phoneNumber: "+2341234556799"),
+  Contact(firstName: "Aladin", phoneNumber: "+2341234556799"),
+  Contact(firstName: "Aladin", phoneNumber: "+2341234556799"),
 ];
