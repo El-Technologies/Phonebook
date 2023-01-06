df5acd0dddfc88adcde8f031c9f30c1c33c32ce3
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-06 13:05:49 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -1,14 +1,45 @@
 import 'contact.dart';
 
 List<Contact> contacts = [
-  Contact(firstName: "Aladin", phoneNumber: "+234 123 4567 890"),
-  Contact(firstName: "Sam", phoneNumber: "+234 123 4567 890"),
-  Contact(firstName: "James", phoneNumber: "+234 123 4567 890"),
-  Contact(firstName: "Silver", phoneNumber: "+234 123 4567 890"),
-  Contact(firstName: "Josh", phoneNumber: "+234 123 4567 890"),
-  Contact(firstName: "Carie", phoneNumber: "+234 123 4567 890"),
-  Contact(firstName: "Zen", phoneNumber: "+234 123 4567 890"),
-  Contact(firstName: "Dominion", phoneNumber: "+234 123 4567 890"),
-  Contact(firstName: "Derrick", phoneNumber: "+234 123 4567 890"),
-  Contact(firstName: "Fred", phoneNumber: "+234 123 4567 890"),
+  Contact(
+    firstName: "Aladin",
+    lastName: "Doe",
+    phoneNumber: "+234 123 4567 890",
+  ),
+  Contact(
+    firstName: "Sam",
+    phoneNumber: "+234 123 4567 890",
+  ),
+  Contact(
+    firstName: "James",
+    phoneNumber: "+234 123 4567 890",
+  ),
+  Contact(
+    firstName: "Silver",
+    phoneNumber: "+234 123 4567 890",
+  ),
+  Contact(
+    firstName: "Josh",
+    phoneNumber: "+234 123 4567 890",
+  ),
+  Contact(
+    firstName: "Carie",
+    phoneNumber: "+234 123 4567 890",
+  ),
+  Contact(
+    firstName: "Zen",
+    phoneNumber: "+234 123 4567 890",
+  ),
+  Contact(
+    firstName: "Dominion",
+    phoneNumber: "+234 123 4567 890",
+  ),
+  Contact(
+    firstName: "Derrick",
+    phoneNumber: "+234 123 4567 890",
+  ),
+  Contact(
+    firstName: "Fred",
+    phoneNumber: "+234 123 4567 890",
+  ),
 ];
