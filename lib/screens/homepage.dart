9b65a3dd0dd3f688b3ed15c2fada10d6cac6d309
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-06 20:06:14 +0100

    commit

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:phonebook/data.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    contacts.sort((a, b) => a.firstName.compareTo(b.firstName));
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {});
+        },
+        child: Icon(
+          Icons.add,
+          size: 20.sp,
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const CustomAppBar(),
+            SizedBox(height: 10.h),
+            const CustomSearchBar(),
+            SizedBox(height: 10.h),
+            Expanded(
+              child: ListView.builder(
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Card(
+                      shape: const CircleBorder(),
+                      child: Container(
+                        height: 40.w,
+                        width: 40.w,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      "${contacts[index].firstName} ${contacts[index].lastName ??= ""}",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: contacts[index].email != null
+                        ? Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                contacts[index].email!,
+                                style: TextStyle(
+                                  fontSize: 10.sp,
+                                ),
+                              ),
+                              Text(contacts[index].phoneNumber),
+                            ],
+                          )
+                        : Text(contacts[index].phoneNumber),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
