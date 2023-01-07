e0ec2ade6ddfdfc142da6fd708529be0d58bf1bc
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-07 07:41:46 +0100

    commit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:phonebook/contact.dart';
 import 'package:phonebook/custom/appbar.dart';
 import 'package:phonebook/custom/searchbar.dart';
 import 'package:phonebook/data.dart';
@@ -14,9 +13,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final TextEditingController _searchController = TextEditingController();
-
-  List<Contact> _searchResults = [];
   @override
   Widget build(BuildContext context) {
     contacts.sort((a, b) => a.firstName.compareTo(b.firstName));
@@ -54,47 +50,7 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             SizedBox(height: 10.h),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: SizedBox(
-                height: 50,
-                child: Stack(
-                  clipBehavior: Clip.none,
-                  children: [
-                    TextField(
-                      controller: _searchController,
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(30.r)),
-                        fillColor: const Color.fromARGB(255, 230, 228, 228),
-                        filled: true,
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: 17.77.h,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      right: 5,
-                      top: 5,
-                      child: Container(
-                        width: 35.w,
-                        height: 35.w,
-                        decoration: const BoxDecoration(
-                          color: Colors.blue,
-                          shape: BoxShape.circle,
-                        ),
-                        child: const Icon(
-                          Icons.search_rounded,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            const CustomSearchBar(),
             SizedBox(height: 10.h),
             Expanded(
               child: ListView.builder(
