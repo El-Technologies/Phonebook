984ce111b24647fd8a4ad0e93999b296952826ae
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-01-15 18:14:47 +0100

    commit

--- lib/data.dart
+++ lib/data.dart
@@ -20,32 +20,39 @@ List<Contact> contacts = [
     phoneNumber: "+234 123 4567 890",
   ),
   Contact(
+    pic: "assets/images/silver.jpeg",
     firstName: "Silver",
     email: "silver@gmail.com",
     phoneNumber: "+234 123 4567 890",
   ),
   Contact(
+    pic: "assets/images/josh.jpeg",
     firstName: "Josh",
     phoneNumber: "+234 123 4567 890",
   ),
   Contact(
+    pic: "assets/images/carie.jpg",
     firstName: "Carie",
     email: "carie@gmail.com",
     phoneNumber: "+234 123 4567 890",
   ),
   Contact(
+    pic: "assets/images/zen.jpeg",
     firstName: "Zen",
     phoneNumber: "+234 123 4567 890",
   ),
   Contact(
+    pic: "assets/images/dominion.jpeg",
     firstName: "Dominion",
     phoneNumber: "+234 123 4567 890",
   ),
   Contact(
+    pic: "",
     firstName: "Derrick",
     phoneNumber: "+234 123 4567 890",
   ),
   Contact(
+    pic: "",
     firstName: "Fred",
     phoneNumber: "+234 123 4567 890",
   ),
